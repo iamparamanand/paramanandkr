787653d513ad18e6aa050fc4e73f946c12b2ae61
Paramanand Kumar <paramanandkr@gmail.com>
2020-08-19 19:20:15 +0530

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { EducationComponent } from './education/education.component';
+import { ResumeComponent } from './resume/resume.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { MapComponent } from './map/map.component';
+import { ContactComponent } from './contact/contact.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '',  component: HomeComponent },
+  { path: 'about',  component: AboutComponent },
+  { path: 'education',  component: EducationComponent },
+  { path: 'resume',  component: ResumeComponent },
+  { path: 'gallery',  component: GalleryComponent },
+  { path: 'map',  component: MapComponent },
+  { path: 'contact',  component: ContactComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    EducationComponent,
+    ResumeComponent,
+    GalleryComponent,
+    MapComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
